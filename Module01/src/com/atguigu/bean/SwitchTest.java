77500c16c8a5fb2923139de927ea59cfc04584fe
wenyueyan <837844121@qq.com>
2021-08-07 20:08:07 +0800

    添加了SwitchTest

---
+++ Module01/src/com/atguigu/bean/SwitchTest.java
@@ -0,0 +1,41 @@
+package com.atguigu.bean;
+
+import java.util.Scanner;
+
+public class SwitchTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入月份");
+        int month = scanner.nextInt();
+        System.out.println("请输入日期");
+        int day = scanner.nextInt();
+        int sumDays = 0;
+        switch (month) {
+            case 12:
+                sumDays += 30;
+            case 11:
+                sumDays += 31;
+            case 10:
+                sumDays += 30;
+            case 9:
+                sumDays += 31;
+            case 8:
+                sumDays += 31;
+            case 7:
+                sumDays += 30;
+            case 6:
+                sumDays += 31;
+            case 5:
+                sumDays += 30;
+            case 4:
+                sumDays += 31;
+            case 3:
+                sumDays += 28;
+            case 2:
+                sumDays += 31;
+            case 1:
+                sumDays += day;
+        }
+        System.out.println("该日期是2019年的第" + sumDays + "天");
+    }
+}
